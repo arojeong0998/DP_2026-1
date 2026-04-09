a1f7b84e9e8ff64fe8065b7ac6456b3861f1bfd9
정아로 <dkakehrmrjs@duksung.ac.kr>
2026-04-09 10:33:38 +0900

    chore: commit all files

---
+++ src/hw/ch05/ch05_lazy/Main.java
@@ -0,0 +1,29 @@
+package hw.ch05.ch05_lazy;
+
+public class Main extends Thread {
+    public static void main(String[] args) {
+        System.out.println("========== 방식 2: Synchronized Lazy ==========");
+        LoggerSynchronizedLazy logger2a = LoggerSynchronizedLazy.getInstance();
+        LoggerSynchronizedLazy logger2b = LoggerSynchronizedLazy.getInstance();
+        logger2a.log("Synchronized Message 1");
+        logger2b.log("Synchronized Message 2");
+        if (logger2a == logger2b) {
+            System.out.println("✓ 동일한 인스턴스");
+        } else {
+            System.out.println("✗ 다른 인스턴스");
+        }
+        System.out.println("\n========== 로그 출력 2 ==========");
+        System.out.println("[LoggerSynchronizedLazy 로그]");
+        System.out.println(logger2a.getLog());
+    }
+
+    @Override
+    public void run() {
+        LoggerSynchronizedLazy obj = LoggerSynchronizedLazy.getInstance();
+        System.out.println(getName() + ": obj = " + obj);
+    }
+
+    public Main(String name) {
+        super(name);
+    }
+}
